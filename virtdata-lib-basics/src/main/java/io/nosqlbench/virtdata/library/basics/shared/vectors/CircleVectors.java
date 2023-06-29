b2d8645130e566026390ab8bb52e43b12ce42f42
Mark Wolters <mwolters138@system76-pc.localdomain>
2023-06-29 09:46:59 -0400

    adding unit tests

--- virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/vectors/CircleVectors.java
+++ virtdata-lib-basics/src/main/java/io/nosqlbench/virtdata/library/basics/shared/vectors/CircleVectors.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2023 nosqlbench
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.nosqlbench.virtdata.library.basics.shared.vectors;
 
 import io.nosqlbench.virtdata.api.annotations.Categories;
@@ -28,4 +44,13 @@ public class CircleVectors implements LongFunction<List<Object>> {
     public List<Object> apply(long value) {
         return algorithm.getVector((value % circleCount), circleCount);
     }
+
+    public int getCircleCount() {
+        return circleCount;
+    }
+
+    public CircleAlgorithm getAlgorithm() {
+        return algorithm;
+    }
+
 }
